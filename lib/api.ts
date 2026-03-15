79823051463dd7fde0b1b626ee4a4673a66b663a
Lina <linaskrobot@gmail.com>
2026-03-15 13:22:29 +0000

    Move API base URL to environment variable

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://car-rental-api.goit.global",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 export default api;
